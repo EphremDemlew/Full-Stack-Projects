a2d65e1bcdbdbbd6bc1f0d1a8cf934682f6cf16d
EphremDemlew <ephyaabb@gmail.com>
2022-06-10 09:20:45 +0300

    part1: exercise 1.1-1.2 done

--- part1/part1/src/components/Content.jsx
+++ part1/part1/src/components/Content.jsx
@@ -1,10 +1,10 @@
 const Content = (props) => {
     return (
-      <div>
+      <>
             {props.part1} {props.exercise1}
             {props.part2} {props.exercise2}
             {props.part3} { props.exercise3}
-      </div>
+      </>
     )
   }
 
