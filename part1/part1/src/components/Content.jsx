fc177b0ba5e1a72a55932a11dc15038c66be392c
EphremDemlew <ephyaabb@gmail.com>
2022-06-10 08:44:19 +0300

    updated the components

---
+++ part1/part1/src/components/Content.jsx
@@ -0,0 +1,11 @@
+const Content = (props) => {
+    return (
+      <div>
+            {props.part1} {props.exercise1}
+            {props.part2} {props.exercise2}
+            {props.part3} { props.exercise3}
+      </div>
+    )
+  }
+
+export default Content;
---
+++ part1/part1/src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Hel = (props) => {
+    return (
+        <div>
+            <h1>{props.course}</h1>
+            <h1>{props.name}</h1>
+    </div>
+)
+}
+
+export default Hel;
